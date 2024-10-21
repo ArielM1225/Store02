239c9b829d9af57f221e38a443796308306646a7
ArielM1225 <ariemeneses243@gmail.com>
2024-10-21 15:32:43 -0300

    Agregado de query para crear la Base de datos, tablas y sus respectivos triggers

---
+++ Store02/DatabaseQuery/STORE02.sql
@@ -0,0 +1,113 @@
+CREATE DATABASE STORE02
+
+USE STORE02
+
+CREATE TABLE Products (
+ProductID INT IDENTITY(1, 1) PRIMARY KEY,
+NameProduct NVARCHAR(100) NOT NULL,
+DescriptionProduct NVARCHAR(255),
+Price DECIMAL(10, 2) NOT NULL,
+Stock INT NOT NULL DEFAULT 0 CHECK (Stock >=0),
+CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+UpdatedAt DATETIME NULL
+);
+
+
+CREATE TABLE Customers (
+CustomerID INT IDENTITY(1, 1) PRIMARY KEY,
+FirstName NVARCHAR(100) NOT NULL,
+LastName NVARCHAR(100) NOT NULL,
+Email NVARCHAR(100) NOT NULL UNIQUE,
+PhoneNumber NVARCHAR(15),
+AddressCustomer NVARCHAR(255),
+City NVARCHAR(100),
+PostalCode NVARCHAR(10),
+Country NVARCHAR(100),
+CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+UpdatedAt DATETIME NULL
+);
+
+
+CREATE TABLE Suppliers (
+SupplierID INT IDENTITY(1, 1) PRIMARY KEY,
+SupplierName VARCHAR(100) NOT NULL,
+ContactInfo NVARCHAR(255),
+CreatedAt DATETIME NULL DEFAULT GETDATE(),
+UpdatedAt DATETIME NULL
+);
+
+
+CREATE TABLE OrderPurchase (
+OrderPID INT IDENTITY(1, 1) PRIMARY KEY,
+SupplierID INT NOT NULL,
+OrderDate DATETIME NOT NULL DEFAULT GETDATE(),
+TotalAmount DECIMAL(10, 2) NOT NULL DEFAULT 0,
+StatusOrder NVARCHAR(20) NOT NULL CHECK (StatusOrder IN ('Pending', 'Completed', 'Canceled')),
+FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID),
+);
+
+
+CREATE TABLE OrderSale (
+OrderSID INT IDENTITY(1, 1) PRIMARY KEY,
+CustomerID INT NOT NULL,
+OrderDate DATETIME NOT NULL DEFAULT GETDATE(),
+TotalAmount DECIMAL(10, 2) NOT NULL DEFAULT 0,
+StatusOrder NVARCHAR(20) NOT NULL CHECK (StatusOrder IN ('Pending', 'Completed', 'Canceled')),
+FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+);
+
+
+CREATE TABLE OrderPurchaseDetail (
+    OrderDetailID INT IDENTITY(1, 1) PRIMARY KEY,
+    OrderPID INT NOT NULL, -- Clave foránea a Order_Purchase
+    ProductID INT NOT NULL, -- Clave foránea a Product
+    Quantity INT NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (OrderPID) REFERENCES OrderPurchase(OrderPID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+
+CREATE TABLE OrderSaleDetail (
+    OrderDetailID INT IDENTITY(1, 1) PRIMARY KEY,
+    OrderSID INT NOT NULL, -- Clave foránea a Order_Sale
+    ProductID INT NOT NULL, -- Clave foránea a Product
+    Quantity INT NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (OrderSID) REFERENCES OrderSale(OrderSID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+
+CREATE TABLE OrderHistory (
+    OrderHistoryID INT IDENTITY(1, 1) PRIMARY KEY,
+    OrderID INT NOT NULL, -- Puede ser OrderID de Order_Sale o Order_Purchase
+    OrderDate DATETIME NOT NULL,
+    TotalAmount DECIMAL(10, 2) NOT NULL,
+    StatusOrder NVARCHAR(20) NOT NULL CHECK (StatusOrder IN ('Pending', 'Completed', 'Canceled')),
+    OrderType NVARCHAR(10) NOT NULL CHECK (OrderType IN ('Sale', 'Purchase')),
+    ProductID INT NOT NULL,
+    Quantity INT NOT NULL CHECK (Quantity > 0),
+    Price DECIMAL(10, 2) NOT NULL CHECK (Price >= 0),
+    HistoryDate DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+
+SELECT * FROM Customers;
+SELECT * FROM Suppliers;
+SELECT * FROM Products;
+SELECT * FROM OrderHistory;
+SELECT * FROM OrderPurchase;
+SELECT * FROM OrderPurchaseDetail;
+
+DROP TABLE OrderHistory;
+DROP TABLE OrderPurchaseDetail
+DROP TABLE OrderSaleDetail
+DROP TABLE OrderPurchase
+DROP TABLE OrderSale
+DROP TABLE Products
+DROP TABLE Customers
+DROP TABLE Suppliers
+
+DROP DATABASE STORE02
+USE master
---
+++ Store02/DatabaseQuery/TRIGGER02.sql
@@ -0,0 +1,198 @@
+USE STORE02
+
+
+--A USAR
+CREATE TRIGGER UpdateStockOnOrder
+ON OrderPurchaseDetail
+AFTER INSERT
+AS
+BEGIN
+
+	-- Aumentar el stock si es una orden de compra ('Purchase') y está 'Completed'
+	IF EXISTS (
+		SELECT 1
+		FROM inserted i
+		INNER JOIN OrderPurchase op ON i.OrderPID = op.OrderPID
+		WHERE i.OrderPID = op.OrderPID AND op.StatusOrder = 'Completed'
+	)
+
+	BEGIN
+		UPDATE p
+		SET p.Stock = p.Stock + i.Quantity
+		FROM Products p
+		INNER JOIN inserted i ON p.ProductID = i.ProductID
+		INNER JOIN OrderPurchase op ON i.OrderPID = op.OrderPID
+		WHERE op.StatusOrder = 'Completed';
+	END
+END;
+
+DROP TRIGGER UpdateStockOnOrder
+
+
+--A USAR
+CREATE TRIGGER UpdateStockOnOrderSale
+ON OrderSaleDetail
+AFTER INSERT
+AS
+BEGIN
+    -- Disminuir stock si es una orden de venta ('Sale') y está 'Pending' o 'Completed'
+    IF EXISTS (SELECT 1 
+               FROM inserted i 
+               INNER JOIN OrderSale os ON i.OrderSID = os.OrderSID 
+               WHERE os.StatusOrder = 'Pending' OR os.StatusOrder = 'Completed')
+    BEGIN
+        UPDATE Products
+        SET Stock = Stock - i.Quantity
+        FROM Products p
+        INNER JOIN inserted i ON p.ProductID = i.ProductID
+        INNER JOIN OrderSale os ON i.OrderSID = os.OrderSID
+        WHERE os.StatusOrder = 'Pending' OR os.StatusOrder = 'Completed'
+          AND p.Stock >= i.Quantity; -- Verificar que haya suficiente stock
+    END
+END;
+
+
+DROP TRIGGER UpdateStockOnOrderSale
+
+
+--A USAR
+CREATE TRIGGER UpdateStockOnStatusChangePurchase
+ON OrderPurchase
+AFTER UPDATE
+AS
+BEGIN
+    -- Verificar si el nuevo estado es 'Canceled' y antes era 'Completed' (para compras)
+    IF EXISTS (SELECT 1 
+               FROM inserted i 
+               JOIN deleted d ON i.OrderPID = d.OrderPID
+               WHERE i.StatusOrder = 'Canceled' AND d.StatusOrder = 'Completed')
+    BEGIN
+        UPDATE Products
+        SET Stock = Stock - od.Quantity
+        FROM Products p
+        INNER JOIN OrderPurchaseDetail od ON p.ProductID = od.ProductID
+        INNER JOIN inserted i ON i.OrderPID = od.OrderPID
+        INNER JOIN deleted d ON d.OrderPID = i.OrderPID
+        WHERE d.StatusOrder = 'Completed';
+    END
+END;
+
+
+DROP TRIGGER UpdateStockOnStatusChangePurchase
+
+
+--A USAR
+CREATE TRIGGER UpdateStockOnStatusChangeSale
+ON OrderSale
+AFTER UPDATE
+AS
+BEGIN
+    -- Verificar si el nuevo estado es 'Canceled' y antes era 'Pending' o 'Completed' (para ventas)
+    IF EXISTS (SELECT 1 
+               FROM inserted i 
+               JOIN deleted d ON i.OrderSID = d.OrderSID
+               WHERE i.StatusOrder = 'Canceled' AND (d.StatusOrder = 'Pending' OR d.StatusOrder = 'Completed'))
+    BEGIN
+        UPDATE Products
+        SET Stock = Stock + od.Quantity
+        FROM Products p
+        INNER JOIN OrderSaleDetail od ON p.ProductID = od.ProductID
+        INNER JOIN inserted i ON i.OrderSID = od.OrderSID
+        INNER JOIN deleted d ON d.OrderSID = i.OrderSID
+        WHERE (d.StatusOrder = 'Pending' OR d.StatusOrder = 'Completed');
+    END
+END;
+
+
+DROP TRIGGER UpdateStockOnStatusChangeSale
+
+
+--A USAR
+CREATE TRIGGER InsertOrderSaleHistory
+ON OrderSale
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO OrderHistory (OrderID, OrderDate, TotalAmount, StatusOrder, OrderType, ProductID, Quantity, Price)
+    SELECT 
+        i.OrderSID,
+        i.OrderDate,
+        i.TotalAmount,
+        i.StatusOrder,
+        'Sale', -- Tipo de orden
+        d.ProductID,
+        d.Quantity,
+        d.Price
+    FROM 
+        inserted i
+    JOIN 
+        OrderSaleDetail d ON i.OrderSID = d.OrderSID; -- Asumiendo que tienes una tabla OrderSaleDetail
+END;
+
+
+--A USAR
+CREATE TRIGGER InsertOrderPurchaseHistory
+ON OrderPurchase
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO OrderHistory (OrderID, OrderDate, TotalAmount, StatusOrder, OrderType, ProductID, Quantity, Price)
+    SELECT 
+        i.OrderPID,
+        i.OrderDate,
+        i.TotalAmount,
+        i.StatusOrder,
+        'Purchase', -- Tipo de orden
+        d.ProductID,
+        d.Quantity,
+        d.Price
+    FROM 
+        inserted i
+    JOIN 
+        OrderPurchaseDetail d ON i.OrderPID = d.OrderPID; -- Asumiendo que tienes una tabla OrderPurchaseDetail
+END;
+
+
+--A USAR
+CREATE TRIGGER UpdateOrderHistoryOnPurchase
+ON OrderPurchase
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO OrderHistory (OrderID, OrderDate, TotalAmount, StatusOrder, OrderType, ProductID, Quantity, Price)
+    SELECT 
+        o.OrderPID, 
+        o.OrderDate, 
+        o.TotalAmount, 
+        o.StatusOrder, 
+        'Purchase', -- Especificar el tipo de orden
+        od.ProductID, 
+        od.Quantity, 
+        od.Price
+    FROM OrderPurchase o
+    JOIN OrderPurchaseDetail od ON o.OrderPID = od.OrderPID
+    JOIN inserted i ON o.OrderPID = i.OrderPID;
+END;
+
+
+
+--A USAR
+CREATE TRIGGER UpdateOrderHistoryOnSale
+ON OrderSale
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO OrderHistory (OrderID, OrderDate, TotalAmount, StatusOrder, OrderType, ProductID, Quantity, Price)
+    SELECT 
+        o.OrderSID, 
+        o.OrderDate, 
+        o.TotalAmount, 
+        o.StatusOrder, 
+        'Sale', -- Especificar el tipo de orden
+        od.ProductID, 
+        od.Quantity, 
+        od.Price
+    FROM OrderSale o
+    JOIN OrderSaleDetail od ON o.OrderSID = od.OrderSID
+    JOIN inserted i ON o.OrderSID = i.OrderSID;
+END;
